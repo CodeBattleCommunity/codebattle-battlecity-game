f4abe1bdee70a2f2bd0ce93c183947d7848f1d59
IzhevskBattleCityDev <Pavel_Mezrin@epam.com>
2018-09-25 18:28:30 +0400

    Remove unused maps / rename

--- games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/model/GameSettingsImpl.java
+++ games/battlecity/src/main/java/com/codenjoy/dojo/battlecity/model/GameSettingsImpl.java
@@ -10,12 +10,12 @@ package com.codenjoy.dojo.battlecity.model;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -89,7 +89,7 @@ public class GameSettingsImpl implements GameSettings {
         List<?> maps = levelRegistry.getAvailableMapsNames();
         map = settings.addSelect("Map", (List<Object>)maps)
                 .type(String.class)
-                .def("warmup");
+                .def("warmup_1");
 
         minAmmoBonusOnMap = settings.addEditBox("Minimum AmmoBonus Bonus On Map").type(Integer.class).def(2);
         maxAmmoBonusOnMap = settings.addEditBox("Maximum AmmoBonus Bonus On Map").type(Integer.class).def(4);
